e70a48cc6e0f43560bf8685f978536849eefc377
brannon <brannon@iterator.net>
2017-06-03 22:49:12 -0600

    Add timestamps to output

--- namecheap.sh
+++ namecheap.sh
@@ -1,38 +1,47 @@
 #!/bin/bash
 
+DATEFMT='+%Y-%m-%dT%H:%M:%S'
+
+fatal() {
+    echo "$(date $DATEFMT) FATAL: $1"
+    exit 1
+}
+
+log() {
+    echo "$(date $DATEFMT) $1"
+}
+
+log "Updating DDNS A record"
+
 IP=$(curl -sSL -H 'Accept: application/json' https://ipinfo.io/ | jq -r '.ip')
 
 if [ -z "$IP" ]; then
-    echo 'Unable to detect IP address'
-    exit 1
+    fatal 'Unable to detect IP address'
 fi
 
-echo "Detected public IP: $IP"
+log "Detected public IP: $IP"
 
 if [ -z "$DDNS_HOST" ]; then
-    echo 'Missing DDNS_HOST variable'
-    exit 1
+    fatal 'Missing DDNS_HOST variable'
 fi
 
 if [ -z "$DDNS_DOMAIN" ]; then
-    echo 'Missing DDNS_DOMAIN variable'
-    exit 1
+    fatal 'Missing DDNS_DOMAIN variable'
 fi
 
 if [ -z "$DDNS_PASSWORD" ]; then
-    echo 'Missing DDNS_PASSWORD variable'
-    exit 1
+    fatal 'Missing DDNS_PASSWORD variable'
 fi
 
 RESULT=$(curl -sSL -H 'Accept: application/json' "https://dynamicdns.park-your-domain.com/update?host=${DDNS_HOST}&domain=${DDNS_DOMAIN}&password=${DDNS_PASSWORD}&ip=${IP}")
 RC=$?
 if [ $RC -ne 0 ]; then
-    echo 'Error updating DDNS A record!'
-    echo 'Response was: '
-    echo $RESULT
-    echo ''
+    log 'Error updating DDNS A record!'
+    log 'Response was: '
+    log $RESULT
+    log ''
     exit $RC
 fi
 
-echo 'DDNS A record updated'
+log 'DDNS A record updated'
 
